f2371735d32385dcf0e521355aa0d319599ae621
Adetunji <jimoholutayo@gmail.com>
2020-11-25 10:09:09 +0100

    first commit

---
+++ valid.js
@@ -0,0 +1,39 @@
+
+
+// var email = document.forms['form']['email'];
+// var password = document.forms['form']['password'];
+
+// var email_error = document.getElementById('email_error');
+// var password_error = document.getElementById('pass_error');
+// email.addEventListener('input text', email_verify);
+// password.addEventListener('input text', pass_verify);
+// function validated(){
+//     if(email.valu.length < 9){
+//         email.style.border="1px solid red";
+//         email_error.style.display = "block";
+//         email.focus();
+//         return false;
+
+//     }
+//     if(pass.valu.length < 6){
+//         pass.style.border="1px solid red";
+//         pass_error.style.display = "block";
+//         pass.focus();
+//         return false;
+//     }
+// }
+// function email_verify(){
+//     if(email.value.length >= 8){
+//        email.style.border = "1px solid red";
+//        email_error.style.display = "none";
+//        email.focus();
+//        return false;
+//     }
+// }
+// function email_verify(){
+//     if(password.value.length >= 5){
+//        password.style.border = "1px solid red";
+//        pass_error.style.display = "none";
+//        return false;
+//     }
+// }
